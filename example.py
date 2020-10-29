d36653ad38ad10442984f11dbc0b5aec081c088f
Frankie <dracko@hotmail.co.uk>
2020-10-29 07:20:48 +0000

    Master branch

---
+++ example.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, request, url_for
+from flask_pymongo import PyMongo
+from os import path
+if path.exists("env.py"):
+  import env 
+
+MONGODB_URI = os.environ.get('MONGO_URI')
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def index():
+    return '''
+        <form method="POST" action="/create" enctype="multipart/form-data">
+            <input type="text" name="username">
+            <input type="file" name"profile_image">
+            <input type="submit">
+        </form>
+    '''
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    if 'profile_image' in request.files:
+        profile_image = request.files['profile_image']
+        mongo.save_file(profile_image.filename, profile_image)
+        mongo.db.users.insert({'username' : request.form.get('username'), 'profile_image_name' : profile_image.filename})
+
+    return 'Done'
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
